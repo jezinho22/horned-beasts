ffcb3958f13ea3787749b4d4e617f49f33704289
Jeremy Johns <jez.johns@gmail.com>
2023-05-02 19:46:08 +0000

    added all props to beast 1

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,26 @@
 import "./App.css";
 
-function HornedBeast({ beastImg, beastName, beastDesc }) {
+function HornedBeast({
+	beastImg,
+	beastName,
+	beastDesc,
+	beastHorns,
+	beastTitle,
+}) {
 	return (
 		<div className="HornedBeast">
 			<h2>{beastName}</h2>
 			<img className="beastImage" src={beastImg} alt="" title="" />
 			<p className="description">{beastDesc}</p>
+			<p className="horns">Horns: {beastHorns}</p>
 		</div>
 	);
 }
 
 export default HornedBeast;
+
+// beastName={beastData[0].keyword}
+// beastImg={beastData[0].image_url}
+// beastTitle={beastData[0].title}
+// beastDesc={beastData[0].description}
+// beastHorns={beastData[0].horns}
--- src/Main.js
+++ src/Main.js
@@ -9,7 +9,9 @@ function Main() {
 				<HornedBeast
 					beastName={beastData[0].keyword}
 					beastImg={beastData[0].image_url}
+					beastTitle={beastData[0].title}
 					beastDesc={beastData[0].description}
+					beastHorns={beastData[0].horns}
 				/>
 			</main>
 		</div>
@@ -17,3 +19,11 @@ function Main() {
 }
 
 export default Main;
+
+// _id: 1,
+// image_url:
+// 	"http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
+// title: "UniWhal",
+// description: "A unicorn and a narwhal nuzzling their horns",
+// keyword: "narwhal",
+// horns: 1,
