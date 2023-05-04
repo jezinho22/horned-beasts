bc10bb4f53ebb6ccda39eff36311329580217a38
Jeremy Johns <jez.johns@gmail.com>
2023-05-04 21:40:07 +0000

    added input box, change handler working

--- src/Search.js
+++ src/Search.js
@@ -1,19 +1,28 @@
-import SearchOptions from "./SearchOptions";
+import { useState } from "react";
+import SearchResults from "./SearchResults";
 import { beastData } from "./data";
 
 function Search() {
+	const [results, setresults] = useState({});
+	// let letters = new RegExp("ar","gi")
+
+	function handleSearch(event) {
+		let searchString = event.target.value;
+		let resultsArray = beastData.filter((beast) =>
+			findString(searchString, beast)
+		);
+		setresults(resultsArray);
+	}
+
+	function findString(searchString, obj) {
+		let word = obj.keyword;
+		return word.match(searchString);
+	}
+
 	return (
-		<div>
-			<form>
-				<button>Search</button>
-				<select name="search" id="search">
-					{beastData.map((beast) => {
-						return <SearchOptions beast={beast.keyword} />;
-					})}
-					<SearchOptions />
-				</select>
-			</form>
+		<div className="search">
 			Search
+			<input type="text" name="search" id="search" onChange={handleSearch} />
 		</div>
 	);
 }
---
+++ src/SearchResults.js
@@ -0,0 +1,9 @@
+function SearchResults({ beast }) {
+	return (
+		// provide the options for the select box
+
+		<option value={beast}>{beast}</option>
+	);
+}
+
+export default SearchResults;
